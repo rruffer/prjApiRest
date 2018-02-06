6d80e8932d90c5d189bc253f9616e89276fa377f
rodolfo.ruffer <rodolforuffer@yahoo.com.br>
2018-02-06 11:15:52 -0200

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/api/PrjApiRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjApiRestApplication.class, args);
+	}
+}
